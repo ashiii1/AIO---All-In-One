1f882727fd8366569fc841f81aaa259f335a6f37
ashiii1 <aashishaik526@gmail.com>
2024-08-12 21:46:03 +0530

    d

--- aio/src/cards/FoodOrder/Components/ExploreMenu/ExploreMenu.jsx
+++ aio/src/cards/FoodOrder/Components/ExploreMenu/ExploreMenu.jsx
@@ -4,14 +4,14 @@ import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = ({category, setCategory}) => {
   return (
-    <div className="explore-menu flex flex-col gap-5" id="explore-menu">
+    <div className="exploref-menu flex flex-col gap-5" id="explore-menu">
       <h1 className="menu-text text-3xl font-[500] text-[#262626]">Explore our menu</h1>
       <p className="lg:max-w-[60%] sm:max-w-full text-[#808080]">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid ipsam
         veniam voluptatibus quia exercitationem velit quod quae officia unde,
         suscipit
       </p>
-      <div className="explore-menu-list flex justify-between items-center gap-[30px] text-center m-[20px.0px] overflow-x-scroll">
+      <div className="exploref-menu-list flex justify-between items-center gap-[30px] text-center m-[20px.0px] overflow-x-scroll">
         {menu_list.map((item,index)=>{
             return (
             <div onClick={()=>setCategory(prev=> prev===item.menu_name?"All":item.menu_name)} key={index} className="menu-list-item">
