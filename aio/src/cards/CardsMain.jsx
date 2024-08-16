90dddac03abb483cdbcdbb0482152ab1803ff779
ashiii1 <aashishaik526@gmail.com>
2024-08-16 21:19:04 +0530

    banners added

--- aio/src/cards/CardsMain.jsx
+++ aio/src/cards/CardsMain.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import  { useEffect, useState } from 'react';
 import MainProd from './MainProd';
+import AliceCarousel from "react-alice-carousel";
+
 
 
 const banners = [
-  'https://static.vecteezy.com/system/resources/thumbnails/002/006/605/small/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-pink-backgroud-for-banner-market-ecommerce-free-vector.jpg',
-  'https://www.shutterstock.com/image-photo/fruits-vegetables-on-green-blurred-260nw-1426818593.jpg',
-  'https://www.shutterstock.com/image-vector/banner-books-concept-education-learning-260nw-2331880685.jpg',
-  'https://cdn.vectorstock.com/i/preview-1x/57/56/template-banner-for-online-store-with-shopping-vector-42935756.jpg',
-  'https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX31859615.jpg'
-];
+  'https://img.freepik.com/free-vector/flat-design-fashion-boutique-twitch-banner_23-2149314982.jpg?semt=ais_hybrid',
+  'https://img.freepik.com/free-vector/gradient-boutique-twitch-banner_23-2149334951.jpg',
+  'https://img.freepik.com/free-vector/flat-design-minimal-boutique-twitch-banner-template_23-2149303608.jpg',
+  ];
 
 const cardsData = [
   { imageSrc: 'https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F9b4ae874-8ae2-11ee-87cc-5c1387c06794.jpg?crop=1600%2C900%2C0%2C0&resize=1200', title: 'Books', description: 'Best book store', link: '/books' },
   { imageSrc: 'https://5.imimg.com/data5/LE/QK/KP/IOS-76296248/product-jpeg-1000x1000.png', title: 'Furniture', description: 'Exchange your furniture', link: '/furnituree' },
-  { imageSrc: 'https://market.todaypricerates.com/images/gocery.jpg', title: 'Grocery', description: 'Fresh groceries available', link: '/grocery' },
-  { imageSrc: 'https://www.weddingsutra.com/images/Vendor_Images/Bridal_Jewellery/Kalyan_Jewellers/Muslim_Nikkah.jpg', title: 'Makeup', description: 'Elegant Makeup Products', link: '/makeup' },
+  { imageSrc: 'https://img.pikbest.com/origin/06/05/45/56ipIkbEsTFMk.jpg!bw700', title: 'Grocery', description: 'Fresh groceries available', link: '/grocery' },
+  { imageSrc: 'http://c8.alamy.com/comp/J516JC/beauty-shop-business-card-design-banner-cosmetics-J516JC.jpg', title: 'Makeup', description: 'Elegant Makeup Products', link: '/makeup' },
   { imageSrc: 'https://boutiquestoredesign.com/wp-content/uploads/2018/09/clothes-store-floating-wall-display-shelves-design-ideas-for-retail.jpg', title: 'Shopping', description: 'Trendy clothes and accessories', link: '/shopping' },
   { imageSrc: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg', title: 'To Let', description: 'Get into your new house', link: '/tolet' },
   { imageSrc: 'https://tse3.mm.bing.net/th?id=OIP.YTTvKnlFKw5hlbwIGYTo9AAAAA&pid=Api&P=0&h=180', title: 'Foodorder', description: 'Get into your new house', link: '/food-order' },
@@ -24,20 +24,23 @@ const cardsData = [
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-400 text-white  text-xl p-3 shadow-md">
-      <ul className="flex justify-around">
-        <li className='bg-red-500 font-bold text-2xl'>ASHI</li>
-        <li className="hover:text-black"><a href="/books">Books</a></li>
-        <li className="hover:text-black"><a href="/furnituree">Furniture</a></li>
-        <li className="hover:text-black"><a href="/shopping">Shopping</a></li>
-        <li className="hover:text-black"><a href="/makeup">Makeup</a></li>
-        <li className="hover:text-black"><a href="/grocery">Grocery</a></li>
-        <li className="hover:text-black"><a href="/tolet">To Let</a></li>
-        <li className="hover:text-black"><a href="/food-order">Food Order</a></li>
-        <li className="hover:text-black"><a href="/secondhand">Second Hand</a></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="bg-red-300 text-black text-xl p-1 shadow-md">
+        <ul className="flex justify-around p-2 space-x-4">
+          <li className="bg-red-400 font-bold text-2xl">ASHI</li>
+          <li className="hover:text-black"><a href="/books">Books</a></li>
+          <li className="hover:text-black"><a href="/furniture">Furniture</a></li>
+          <li className="hover:text-black"><a href="/shopping">Shopping</a></li>
+          <li className="hover:text-black"><a href="/makeup">Makeup</a></li>
+          <li className="hover:text-black"><a href="/grocery">Grocery</a></li>
+          <li className="hover:text-black"><a href="/tolet">To Let</a></li>
+          <li className="hover:text-black"><a href="/food-order">Food Order</a></li>
+          <li className="hover:text-black"><a href="/secondhand">Second Hand</a></li>
+        </ul>
+      </nav>
+    </div>
   );
+  
 };
 
 const BannerSlider = () => {
@@ -52,11 +55,11 @@ const BannerSlider = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-96 overflow-hidden ">
-      {banners.map((banner, index) => (
+<div className="relative w-full h- pt-2 overflow-hidden">
+{banners.map((banner, index) => (
         <img
           key={index}
-          className={`absolute w-full h-96 object-cover transition-opacity duration-1000 ${index === currentBannerIndex ? 'opacity-100' : 'opacity-0'}`}
+          className={`absolute w-full  object-cover transition-opacity duration-1000 ${index === currentBannerIndex ? 'opacity-100' : 'opacity-0'}`}
           src={banner}
           alt={`Banner ${index + 1}`}
           style={{ left: `${index === currentBannerIndex ? 0 : 100}%` }}
@@ -68,8 +71,8 @@ const BannerSlider = () => {
 
 const CardsMain = () => {
   return (
-    <div className="p-8 bg-gray-100">
-      <h2 className="text-center text-4xl pb-3 font-bold mb-8 text-black">Featured Categories</h2>
+    <div className="p-8 bg-white">
+      <h2 className="text-center text-4xl pb-3 font-bold mb-8 text-black">Categories</h2>
       <div className="grid grid-cols-1 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {cardsData.map((card, index) => (
           <div
@@ -94,15 +97,51 @@ const CardsMain = () => {
   );
 };
 
-const LandingPage = () => {
+
+function LandingPage({ products = [] }) {
+  const banners = [
+    "https://i.pinimg.com/originals/cc/b3/4f/ccb34f51bba6597deec3bf36ed654315.gif",
+    "https://static.vecteezy.com/system/resources/previews/006/633/040/original/online-shopping-spring-on-phone-flower-pink-big-sale-banner-marketing-poster-fashion-vector.jpg",
+  "https://assets.limetray.com/assets/image_manager/uploads/7639/eb-slide-23423-3.jpg"
+  ];  
+  
+  
+  
+  
   return (
     <div>
       <Navbar />
+
+<main
+      id="home"
+      className="containe mx-auto mb-5 px-2 mt-5 md:mt-[110px]"
+    >
+      {/* Banner Carousel */}
+      <div className="mb-4 w-full h-[50vh]  overflow-hidden"> {/* Set height to 50% of viewport height */}
+        <AliceCarousel
+          autoPlay
+          autoPlayInterval={3000}
+          infinite
+          disableDotsControls
+          disableButtonsControls
+          mouseTracking
+          items={banners.map((banner, index) => (
+            <div key={index} className="w-full h-full"> {/* Full width and height */}
+              <img
+                src={banner}
+                alt="Banner"
+                className="w-full h-full object-contain" 
+                style={{ margin: 0, padding: 0 }}             />
+            </div>
+          ))}
+        />
+      </div>
+</main>
       <BannerSlider />
       <CardsMain />
       <MainProd/>
     </div>
   );
-};
+}
 
 export default LandingPage;
--- aio/src/cards/Makeup/MakeupMain.jsx
+++ aio/src/cards/Makeup/MakeupMain.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const MakeupMain = () => {
   return (
-    <div>MakeupMain</div>
-  )
+    <>
+    
+    <div className="text-center font-bold text-6xl pt-24">Makeup and seconhand are Under Development please explore other categories</div>
+  
+  </>)
+
 }
 
 export default MakeupMain
